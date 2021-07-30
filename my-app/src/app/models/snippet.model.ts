67a997c4a572df48af010827e7d39fc828bf7cf3
Elena Zabalueva <age_eva@mail.ru>
2021-07-30 14:14:51 +0300

    feat: add model for snippet

---
+++ my-app/src/app/models/snippet.model.ts
@@ -0,0 +1,24 @@
+import { Thumbnail } from "./thumbnail.model";
+
+export interface Snippet {
+    "publishedAt": string,
+    "channelId": string,
+    "title": string,
+    "description": string,
+    "thumbnails": {
+        "default": Thumbnail,
+        "medium": Thumbnail,
+        "high": Thumbnail,
+        "standard": Thumbnail,
+        "maxres": Thumbnail
+    },
+    "channelTitle": string,
+    "tags": string[],
+    "categoryId": string,
+    "liveBroadcastContent": string,
+    "localized": {
+        "title": string,
+        "description": string
+    },
+    "defaultAudioLanguage": string
+}
