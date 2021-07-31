5b8c6399df4715e0ce468d9114a102b6635d2c8c
Elena Zabalueva <age_eva@mail.ru>
2021-07-31 14:17:48 +0300

    feat: create header component

--- my-app/src/app/components/components.module.ts
+++ my-app/src/app/components/components.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { HeaderComponent } from './header/header/header.component';
 
 @NgModule({
   declarations: [
-    SearchItemComponent
+    SearchItemComponent,
+    SearchResultsComponent,
+    HeaderComponent,
   ],
   imports: [
     CommonModule,
