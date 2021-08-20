17c4707feccd7dbe890928fad7a9c2f74fa92a99
Elena Zabalueva <age_eva@mail.ru>
2021-08-20 12:51:02 +0300

    feat: add mockdata for search result

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +10,7 @@ import { ComponentsModule } from './components/components.module';
     AppComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     ComponentsModule,
     BrowserAnimationsModule,
--- my-app/src/app/components/search/search-results/search-results.component.ts
+++ my-app/src/app/components/search/search-results/search-results.component.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { SearchItem } from 'src/app/models/searchItem.model';
 import { SearchResults } from 'src/app/models/searchResults.model';
 import { ShowResultService } from 'src/app/services/showResultService';
+import { map } from 'rxjs/operators';
 /* import { Page } from 'src/app/models/page.model'; */
 /* import { PageInfo } from 'src/app/models/pageInfo.model'; */
 
@@ -17,8 +19,15 @@ export class SearchResultsComponent {
 
   searchItems: SearchItem[] = [];
 
-  constructor(showResultService: ShowResultService) {
+  constructor(showResultService: ShowResultService, private http: HttpClient) {
     this.searchResult = {} as SearchResults;
+    this.http
+      .get("/assets/mockdata.json")
+      .pipe(map((data:any) => data))
+      .subscribe((data:any) => {
+        this.searchResult = data;
+        console.log(data);
+      });
     showResultService.showResult$.subscribe((show: boolean) => this.showResultFlag = show);
   }
 }
