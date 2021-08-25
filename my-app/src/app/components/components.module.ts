fa3498885356fcdd9e893ddc50a451277a14a740
Elena Zabalueva <ezabalueva@avaya.com>
2021-08-25 22:54:37 +0300

    fix: correct pipe for sort by word

--- my-app/src/app/components/components.module.ts
+++ my-app/src/app/components/components.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { SearchItemComponent } from './search/search-item/search-item.component';
 import { SearchResultsComponent } from './search/search-results/search-results.component';
 import { HeaderComponent } from './header/header.component';
 import MaterialsModule from '../materials/materials.module';
 import { SortingComponent } from './sorting/sorting.component';
 import { HighlightBorderDirective } from '../directives/highlight-border.directive';
-import { SortPipe } from '../pipes/sort.pipe';
-import { FormsModule } from '@angular/forms';
+import { FilterByWordPipe } from '../pipes/filter-by-word.pipe';
 
 @NgModule({
-    declarations: [
-        SearchItemComponent,
-        SearchResultsComponent,
-        HeaderComponent,
-        SortingComponent,
-        HighlightBorderDirective,
-        SortPipe,
-    ],
-    imports: [CommonModule, MaterialsModule, FormsModule],
-    exports: [
-        SearchItemComponent,
-        SearchResultsComponent,
-        HeaderComponent,
-        HighlightBorderDirective,
-        SortPipe,
-    ],
+  declarations: [
+    SearchItemComponent,
+    SearchResultsComponent,
+    HeaderComponent,
+    SortingComponent,
+    HighlightBorderDirective,
+    FilterByWordPipe,
+  ],
+  imports: [CommonModule, MaterialsModule, FormsModule],
+  exports: [
+    SearchItemComponent,
+    SearchResultsComponent,
+    HeaderComponent,
+    HighlightBorderDirective,
+    FilterByWordPipe,
+  ],
 })
 export class ComponentsModule {}
